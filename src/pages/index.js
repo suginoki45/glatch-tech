5ea9e1afdd5b852b0b13c8ef71279909b09d6d12
Yuki Sugitani <yuki.sugitani@glatchdesign.com>
2019-04-02 12:12:58 +0900

    GraphQLのインポートを追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import PostLink from "../components/post-link"
 import SEO from "../components/seo"
