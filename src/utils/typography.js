6e6c490c50b6763d8cdc98ef0d7de93d65225ee0
Yuki Sugitani <yuki.sugitani@glatchdesign.com>
2019-04-07 18:34:57 +0900

    Typography.jsを導入

---
+++ src/utils/typography.js
@@ -0,0 +1,20 @@
+import Typography from "typography"
+
+const typography = new Typography({
+  baseFontSize: "18px",
+  baseLineHeight: 1.75,
+  bodyFontFamily: [
+    "-apple-system",
+    "BlinkMacSystemFont",
+    "Helvetica Neue",
+    "游ゴシック体",
+    "Yugothic",
+    "游ゴシック",
+    "Yu Gothic",
+    "Verdana",
+    "メイリオ",
+    "sans-serif",
+  ],
+})
+
+export default typography
