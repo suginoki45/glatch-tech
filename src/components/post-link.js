73734188d1597c668b3e4b4380f2bd32577aadf3
Yuki Sugitani <yuki.sugitani@glatchdesign.com>
2019-04-08 18:12:58 +0900

    post-linkコンポーネントにスタイルの追加

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -1,10 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import "./post-link.css"
+
 const PostLink = ({ post }) => (
-  <div>
+  <div className="post">
     <Link to={post.frontmatter.path}>
-      {post.frontmatter.title}
+      <p className="post__title">{post.frontmatter.title}</p>
+      <time className="post__date">{post.frontmatter.date}</time>
     </Link>
   </div>
 )
