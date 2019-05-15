567dc42aa33542b704c813b8d3e8026f0ff47861
Yuki Sugitani <yuki.sugitani@glatchdesign.com>
2019-05-15 16:27:25 +0900

    Update heading margins

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -18,7 +18,13 @@ const typography = new Typography({
   overrideStyles: () => ({
     h2: {
       fontSize: '1.5rem',
-      lineHeight: 1.3
+      lineHeight: 1.3,
+      marginTop: '3rem',
+      marginBottom: '1.5rem'
+    },
+    h3: {
+      marginTop: '2.5rem',
+      marginBottom: '1.25rem'
     }
   })
 })
