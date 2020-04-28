f989f4cda69b656ce5c3f70b9e0d7490cb667421
suginoki45 <yuki.sugitani@glatchdesign.com>
2020-04-28 16:42:47 +0900

    不要なnode_modulesを削除

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,6 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import cx from "classnames"
 import styles from "./header.module.css"
 
 const Header = class extends React.Component {
