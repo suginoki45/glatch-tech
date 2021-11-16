8412867af7457304c95962aa28e3010538bafea6
Yuki Sugitani <yuki.sugitani@glatchdesign.com>
2021-11-16 12:29:50 +0900

    gatsby-remark-code-titlesを追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,12 @@ module.exports = {
       options: {
         plugins: [
           {
+            resolve: `gatsby-remark-code-titles`,
+            options: {
+              className: 'gatsby-remark-code-title',
+            },
+          },
+          {
             resolve: `gatsby-remark-prismjs`,
             options: {
               prompt: {
